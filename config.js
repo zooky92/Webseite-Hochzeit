22aa636e9ce5db0002316eb5965dad2fed7b7855
Denny <denny@example.com>
2026-02-21 13:28:23 +0100

    Update Google Apps Script deployment ID

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Google Apps Script Web App URL
 // Ersetze THIS_WEB_APP_URL mit deiner Google Apps Script URL
-const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwxQ-NeY6VDlKfEv5-8eYDpMxcLN7kLx48A2EBrX4QKPWbrXAQyD_j4IcS_kOA4CoL1HA/exec";
+const GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwXR1XftWriZvxxHfxoBbB5G7pZnUnA_vWMMk9MgwPNsJa8q05fmbsCVnz-ERFwXYhJRw/exec";
